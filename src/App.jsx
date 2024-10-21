c2379c0e69a852588798b8bd61c5fabc453e3df0
tukulcondro <tukuldas68@gmail.com>
2024-10-22 03:18:32 +0600

    add file add

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Header from './Component/Header/header'
+import Blogs from './Component/Blogs/Blogs'
+function App() {
+  
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
